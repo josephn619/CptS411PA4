ec16643df728130cfc1a94f26f64fb32123150c8
(Adam) Joseph Nassar <45108299+josephn619@users.noreply.github.com>
2022-11-03 14:36:04 -0700

    Main update
    Added initial main functionality

--- psuedo.cpp
+++ psuedo.cpp
@@ -1,4 +1,25 @@
-int main(void)
+#include <mpi.h>
+#include <cstdio>
+#include <chrono>
+#include <cmath>
+#include <vector>
+#include <cstdlib>
+#include <time.h>
+
+int main(int argc, char *argv[])
 {
-    
+    srand((unsigned int)time(nullptr));
+
+    int seed = rand() % 10000;
+
+    int rank, p;
+
+    int x0 = atoi(argv[1]);
+    int A = atoi(argv[2]);
+    int B = atoi(argv[3]);
+    int P = atoi(argv[4]);
+
+    MPI_Init(nullptr, nullptr); MPI_Comm_rank(MPI_COMM_WORLD, &rank); MPI_Comm_size(MPI_COMM_WORLD,&p);
+
+    MPI_Finalize();
 }
