e7f264a09d0bae2ab1ad467deb88e12181153865
(Adam) Joseph Nassar <45108299+josephn619@users.noreply.github.com>
2022-11-07 18:27:52 -0800

    update function param
    Added n and sub_vector

--- psuedo.cpp
+++ psuedo.cpp
@@ -6,7 +6,7 @@
 #include <cstdlib>
 #include <time.h>
 
-void serial_baseline(int n) {
+void serial_baseline(int rank, int n, std::vector<int> sub) {
     for (int i = 0; i < n; i++) {
         // x_i = (x_(i-1)*a +1*b) % p
     }
@@ -16,7 +16,7 @@ void serial_matrix() {
     continue;
 }
 
-void matrix_mult_2x2(std::vector<int> A, std::vector<int>B, std::vector<int>RecvBuff, int P) {
+void matrix_mult_2x2(std::vector<int> A, std::vector<int> B, std::vector<int>RecvBuff, int P) {
     RecvBuff[0] = ((A.at(0) * B.at(0)) + (A.at(1) * B.at(3))) % P;
     RecvBuff[1] = ((A.at(0) * B.at(1)) + (A.at(1) * B.at(3))) % P;
     RecvBuff[2] = ((A.at(2) * B.at(0)) + (A.at(3) * B.at(2))) % P;
@@ -31,7 +31,7 @@ int main(int argc, char *argv[]) {
     //assert(p>=1); assert(n%p==0); assert(n>p);
 
     int x0 = atoi(argv[1]); //seed
-    int n = atoi(argv[2]);
+    int n = atoi(argv[2]); //size
     int A = atoi(argv[3]); //constant
     int B = atoi(argv[4]); //constant
     int P = atoi(argv[5]); //constant (PRIME)
