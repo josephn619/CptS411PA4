a04d43582f56dd6f5387ca86586fbd9c0eee2ccf
(Adam) Joseph Nassar <45108299+josephn619@users.noreply.github.com>
2022-11-09 19:53:14 -0800

    Fixed some formats
    Changed some stuff

--- psuedo.cpp
+++ psuedo.cpp
@@ -27,27 +27,11 @@ void serial_baseline(int n, int A, int B, int P, int seed, int randoms[])
 void serial_matrix(int n, int A, int B, int P, int seed, int randoms[]) 
 {
     randoms[0] = seed;
+
     for (int i = 1; i < n; i++)  
     {
         randoms[i] = ((A * randoms[i-1]) + B) % P;
     }
-}
-
-int summation(int A, int n) 
-{
-    int sum = 0;
-    for (int i = 1; i < n; i++) {
-        sum += pow(A,i);
-    }
-    return sum;
-}
-
-void calc_for_i(int A, int B, int i, std::vector<int>RecvBuff) 
-{
-    RecvBuff[0] = pow(A,i);
-    RecvBuff[1] = 0;
-    RecvBuff[2] = B*summation(A, i);
-    RecvBuff[3] = 1;
 }
 
 void matrix_mult_2x2(int A[], int B[], int P) 
@@ -72,15 +56,11 @@ void vector_transformation(int vector[], int matrix[], int P)
 //randoms is n/p where p is number of processors
 void parallel_prefix(int n, int A, int B, int P, int seed, int process_matrix[], int rank, int p)
 {
-    int index = n/p * rank;    
+    int index = n/p * rank, target = 0, i = 1;
     int matrix_power = 1;
-    int I[4]= {1,0,0,1};
-    int world_size;
-    int SendM[] = {A,0,B,1};
-    int RecvM[] = {0,0,0,0};
-    int target = 0;
-    int i = 1;
+    int I[4]= {1,0,0,1}, SendM[4] = {A,0,B,1}, RecvM[4] = {0,0,0,0};
     MPI_Status status;
+    
     for (int j = 2; j <= ((p-1)*n/p); j = j<<1)
     {
         target = (rank ^ i);
@@ -88,7 +68,7 @@ void parallel_prefix(int n, int A, int B, int P, int seed, int process_matrix[],
         MPI_Sendrecv(SendM, 4, MPI_INT, target, 0, RecvM, 4, MPI_INT, target, 0, MPI_COMM_WORLD, &status);
         matrix_mult_2x2(SendM, RecvM, P);
 
-        if ((j) == index)
+        if (j == index)
         {
             process_matrix[0] = SendM[0];
             process_matrix[1] = SendM[1];
@@ -98,19 +78,19 @@ void parallel_prefix(int n, int A, int B, int P, int seed, int process_matrix[],
 
     }
     
-    if (rank==0)
+    if (rank == 0)
     {
         process_matrix[0] = I[0];
         process_matrix[1] = I[1];
         process_matrix[2] = I[2];
         process_matrix[3] = I[3];
     }
+
     //printMatrix(process_matrix, rank);
 }
 
 void calculateRandoms(int process_matrix[], int randoms[], int seed, int rank, int P, int n, int p, int A, int B)
 {
-    
     int vector[2] = {seed, 1};
     int base[4] = {A,0,B,1};
 
@@ -122,8 +102,6 @@ void calculateRandoms(int process_matrix[], int randoms[], int seed, int rank, i
         std::cout << "Rank: " << rank << "\t Random Value: " << randoms[i] << std::endl;
         matrix_mult_2x2(process_matrix, base, P);
     }
-    
-    
 }
 
 int main(int argc, char *argv[]) 
@@ -140,7 +118,6 @@ int main(int argc, char *argv[])
     //int A = atoi(argv[3]); //constant
     //int B = atoi(argv[4]); //constant
     //int P = atoi(argv[5]); //constant (PRIME)
-
 
     MPI_Init(nullptr, nullptr); MPI_Comm_rank(MPI_COMM_WORLD, &rank); MPI_Comm_size(MPI_COMM_WORLD,&p);
 
